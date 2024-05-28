36539f3fd22b18c491a085a2e0f8bb8cf640d462
Roberto Gutiérrez <101654801+robgutsopedra@users.noreply.github.com>
2024-05-28 11:33:19 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "service_accounts_email" {
   value       = google_service_account.service_accounts.email
   description = "The e-mail address of the service account."
 }
+
+output "service_account_email" {
+  value       = google_service_account.service_accounts.email
+  description = "The e-mail address of the service account."
+}
